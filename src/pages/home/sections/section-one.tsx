f4514c1b5f908c443299d13653ffdc49d5b50f3f
Anton Persson <65736652+zoniibtw@users.noreply.github.com>
2024-01-09 10:20:39 +0100

    button fixed on hero page

--- src/pages/home/sections/section-one.tsx
+++ src/pages/home/sections/section-one.tsx
@@ -1,47 +1,62 @@
 import Image from "../../../assets/wepb/hero-bg.webp";
+import { scrollTo } from "../../../utils/scrollTo";
 
 function SectionOne() {
-    return ( 
-        <div className="bg-cover bg-center bg-no-repeat" style={{
-            backgroundImage: `linear-gradient(to bottom, rgba(34, 34, 34, 0.3), rgba(34, 34, 34, 0.3)), url(${Image})`,
-        }}>
-            <div className="h-[92vh] w-full py-[5%] md:px-0 md:w-70 mx-auto max-w-[1700px] flex items-center">
-                <div className="flex flex-col gap-10">
-                    <div className="flex justify-start">
-                        <h1 className="text-[115px] leading-[115px] font-light text-white uppercase">
-                            WELCOME TO<br />
-                            OUR WORLD<br />
-                            WHERE MORE<br />
-                            IS MORE
-                        </h1>
-                    </div>
-                    <div className="w-full h-full flex items-end">
-                        <div className="">
-                            <p className="text-[28px] leading-[30px] text-white font-light w-[60%]">
-                                We have scoured the market for you and offer a highly curated 
-                                selection of our favourite items. These precious pieces come 
-                                from both small, independent businesses as well as established, 
-                                well-known brands. We hope to inspire you with our top picks 
-                                of beautiful things from interior design and homeware to unique 
-                                hotels and spaces.
-                            </p>
-                        </div>
-
-                        <div className="">
-                            <div className="">
-                                <div className="flex flex-col">
-                                    <h2 className="text-[28px] leading-[28px] text-white font-medium">Enter</h2>
-                                    <h3 className="text-[28px] leading-[28px] text-white font-light">Whylessisnotmore</h3>
-                                </div>
-                                
-                                <div className=""></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div
+      className="bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(34, 34, 34, 0.3), rgba(34, 34, 34, 0.3)), url(${Image})`,
+      }}
+    >
+      <div className="h-[92vh] w-full py-[5%] md:px-0 md:w-70 mx-auto max-w-[1700px] flex items-center">
+        <div className="flex flex-col gap-10">
+          <div className="flex justify-start">
+            <h1 className="text-[115px] leading-[115px] font-light text-white uppercase">
+              WELCOME TO<br />
+              OUR WORLD<br />
+              WHERE MORE<br />
+              IS MORE
+            </h1>
+          </div>
+          <div className="w-full h-full flex items-end">
+            <div className="">
+              <p className="text-[28px] leading-[30px] text-white font-light w-[60%]">
+                We have scoured the market for you and offer a highly curated 
+                selection of our favourite items. These precious pieces come 
+                from both small, independent businesses as well as established, 
+                well-known brands. We hope to inspire you with our top picks 
+                of beautiful things from interior design and homeware to unique 
+                hotels and spaces.
+              </p>
             </div>
+            <div className="flex justify-end items-end w-full">
+              <button
+                className="group flex items-end gap-10"
+                onClick={() => scrollTo('interiors')}
+                style={{ color: '#F7F6F2' }}
+              >
+                <div className="flex flex-col items-start">
+                    <p className="text-[28px] leading-[28px] font-normal">Enter</p>
+                    <p className="text-[28px] leading-[28px] font-light">Whylessisnotmore</p>
+                </div>
+                <span className="flex items-center w-24 h-10 border-2 rounded-full overflow-hidden group-hover:bg-white group-hover:text-[var(--background-color)] group-hover:border-[var(--text-color)] transition-colors duration-500 ease-out">
+                  <span className="relative w-full flex items-center justify-center rotate-[0.01deg] transition-transform duration-500 ease-out group-hover:translate-x-[80px]">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6">
+                      <path d="M22.35 12 12 1.65 10.95 2.7l8.54 8.55H1.75v1.5h17.74l-8.55 8.55L12 22.35 22.35 12Z" fill="#fff"></path>
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="h-6 absolute top-0 -left-11">
+                      <path d="M22.35 12 12 1.65 10.95 2.7l8.54 8.55H1.75v1.5h17.74l-8.55 8.55L12 22.35 22.35 12Z"></path>
+                    </svg>
+                  </span>
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default SectionOne;
